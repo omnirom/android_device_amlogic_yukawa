a2cddf1ec67e3fdcf819d0add30119b2eb935256
maxwen <max.weninger@gmail.com>
2021-11-16 17:07:48 +0100

    yukawa: split our changes from common into omni_yukawa
    to make our live easier for upstream merges
    
    Change-Id: I1e75961767eeec926c0c56ecfa798f68d34ca6e5

--- omni_yukawa.mk
+++ omni_yukawa.mk
@@ -36,3 +36,63 @@ PRODUCT_NAME := omni_yukawa
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := VIM3
 PRODUCT_MANUFACTURER := Khadas
+
+# make it a tablet
+PRODUCT_COPY_FILES +=  \
+    frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml
+
+# just set a default - same as kernel default
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.cpufreq.governor=schedutil
+
+#PRODUCT_PROPERTY_OVERRIDES += debug.sf.disable_hwc_overlays=1
+
+# Wifi
+PRODUCT_PROPERTY_OVERRIDES += ro.boot.wificountrycode=00
+
+# gps
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+
+PRODUCT_PACKAGES += \
+    gps.yukawa
+
+PRODUCT_PROPERTY_OVERRIDES += gps.device.path=/dev/ttyACM0
+
+PRODUCT_PROPERTY_OVERRIDES += ro.surface_flinger.use_color_management=false \
+    ro.surface_flinger.has_wide_color_display=false \
+    ro.surface_flinger.has_HDR_display=false
+
+# Additional apps
+PRODUCT_PACKAGES += \
+    DeviceParts \
+    OmniProvision \
+    Terminal
+
+# widevine
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/binaries/drm/vendor/lib/mediadrm/libwvdrmengine.so:$(TARGET_COPY_OUT_VENDOR)/lib/mediadrm/libwvdrmengine.so \
+    $(LOCAL_PATH)/binaries/drm/vendor/lib64/mediadrm/libwvdrmengine.so:$(TARGET_COPY_OUT_VENDOR)/lib64/mediadrm/libwvdrmengine.so \
+    $(LOCAL_PATH)/binaries/drm/vendor/bin/hw/android.hardware.drm@1.3-service.widevine:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.drm@1.3-service.widevine \
+    $(LOCAL_PATH)/binaries/drm/vendor/etc/init/android.hardware.drm@1.3-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.3-service.widevine.rc \
+    $(LOCAL_PATH)/binaries/drm/vendor/lib64/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libwvhidl.so
+
+# Keep the VNDK APEX in /system partition for REL branches as these branches are
+# expected to have stable API/ABI surfaces.
+ifneq (REL,$(PLATFORM_VERSION_CODENAME))
+  PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
+endif
+
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.1-service
+
+# for bringup to disable secure adb - copy adbkey.pub from ~/.android
+#PRODUCT_ADB_KEYS := device/amlogic/yukawa/adbkey.pub
+#PRODUCT_PACKAGES += \
+    adb_keys
+
+#PRODUCT_COPY_FILES += \
+    device/amlogic/yukawa/input/Vendor_1fd2_Product_6003.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_1fd2_Product_6003.idc
