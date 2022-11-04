snappautomotive/device_amlogic_yukawa

Set cec_device_types property
Bug: 154122911
Test: make
Change-Id: I680279b1081e5654c84dd794215774c5df8dbcb4

device-common.mk
@@ -214,6 +214,7 @@ PRODUCT_PACKAGES += \
     hdmi_cec.yukawa
 
 PRODUCT_PROPERTY_OVERRIDES += ro.hdmi.device_type=4 \
+    ro.hdmi.cec_device_types=playback \
     persist.sys.hdmi.keep_awake=false
 
 PRODUCT_COPY_FILES += \

omnirom/android_device_amlogic_yukawa

device-common.mk
@@ -252,6 +252,7 @@ PRODUCT_PACKAGES += \
     hdmi_cec.yukawa
 
 PRODUCT_PROPERTY_OVERRIDES += ro.hdmi.device_type=4 \
+    ro.hdmi.cec_device_types=playback_device \
     persist.sys.hdmi.keep_awake=false
 
 PRODUCT_COPY_FILES += \